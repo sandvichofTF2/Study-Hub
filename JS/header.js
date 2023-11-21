ed2c8734cdc53a5e7080d26ff23d4cae0aa13ba6
sandvichofTF2 <long05202@gmail.com>
2023-11-21 22:36:29 +0700

    tried addin cool header anim

---
+++ JS/header.js
@@ -0,0 +1,11 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
